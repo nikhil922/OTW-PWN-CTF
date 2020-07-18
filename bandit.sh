fa7c3861aaa85deeaee548b7c49f89f630cfd9a8
nikhil922 <nikhil922>
2020-07-18 14:14:13 +0000

    added level 1-2

--- bandit.sh
+++ bandit.sh
@@ -11,15 +11,22 @@ sshCopy(){
 }
 
 autoSSH(){
-	#level 0-1
-	echo "bandit 0"
 	mkdir ./otw/tmp
+
+	#level 0-1
+	echo "bandit 0"	
 	echo "bandit0"> ./otw/tmp/pass
 	sshAccess 0 'ls'
 	sshCopy 0 /home/bandit0/readme ./otw/tmp
 	mv ./otw/tmp/readme ./otw/tmp/pass
 	echo "bandit 1 password is: $(<./otw/tmp/pass)"
-	
+
+	#level 1-2
+	sshAccess 1 'ls'	
+	sshCopy 1 /home/bandit1/./- ./otw/tmp
+	mv ./otw/tmp/./- ./otw/tmp/pass
+	echo "bandit 2 password is: $(<./otw/tmp/pass)"v
+
 	rm -r ./otw/tmp
 }
 autoSSH;
