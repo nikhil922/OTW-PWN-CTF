566cc172c0d6218253cc908264825f20d682d74e
nikhil922 <nikhil922>
2020-07-19 17:25:49 +0000

    removed tmp otw folders for better readability

--- bandit.sh
+++ bandit.sh
@@ -4,31 +4,29 @@
 
 #access server
 sshAccess(){ 
-	sshpass -f ./otw/tmp/pass ssh -o StrictHostKeyChecking=no bandit$1@bandit.labs.overthewire.org -p2220 "$2"
+	sshpass -f ./pass ssh -o StrictHostKeyChecking=no bandit$1@bandit.labs.overthewire.org -p2220 "$2"
 } 
 
 #transfer files from remote server to local system
 sshCopy(){
-	sshpass -f ./otw/tmp/pass scp -P 2220 bandit$1@bandit.labs.overthewire.org:$2 $3
+	sshpass -f ./pass scp -P 2220 bandit$1@bandit.labs.overthewire.org:$2 $3
 }
-
-mkdir ./otw/tmp
 
 #level 0-1
 #pasword is bandit0
 echo "bandit 0"
-echo "bandit0"> ./otw/tmp/pass
+echo "bandit0"> ./pass
 sshAccess 0 'ls'
-sshCopy 0 /home/bandit0/readme ./otw/tmp
-mv ./otw/tmp/readme ./otw/tmp/pass
-echo "bandit 1 password is: $(<./otw/tmp/pass)"
+sshCopy 0 /home/bandit0/readme ./
+mv ./readme ./pass
+echo "bandit 1 password is: $(<./pass)"
 
 #level 1-2
 #password is in a file called -
 sshAccess 1 'ls'	
-sshCopy 1 /home/bandit1/./- ./otw/tmp
-mv ./otw/tmp/./- ./otw/tmp/pass
-echo "bandit 2 password is: $(<./otw/tmp/pass)"
+sshCopy 1 /home/bandit1/./- ./
+mv ././- ./pass
+echo "bandit 2 password is: $(<./pass)"
 
 #level 2-3
 #pasword is in a file with spaces in filename use "" or '' or backslash (spaces\ in\ this\ filename)
@@ -38,17 +36,17 @@ ban2(){
 	cp "spaces in this filename" /tmp/tempY/f0
 }
 sshAccess 2 "$(typeset -f ban2); ban2"
-sshCopy 2 /tmp/tempY/f0 ./otw/tmp
+sshCopy 2 /tmp/tempY/f0 ./
 sshAccess 2 'rm -r /tmp/tempY'
-mv ./otw/tmp/f0 ./otw/tmp/pass
-echo "bandit 3 password is: $(<./otw/tmp/pass)"
+mv ./f0 ./pass
+echo "bandit 3 password is: $(<./pass)"
 
 #level 3-4
 #password is in a hidden file
 sshAccess 3 'ls; cd inhere; ls -a'
-sshCopy 3 /home/bandit3/inhere/.hidden ./otw/tmp
-mv ./otw/tmp/.hidden ./otw/tmp/pass
-echo "bandit 4 password is: $(<./otw/tmp/pass)"
+sshCopy 3 /home/bandit3/inhere/.hidden ./
+mv ./.hidden ./pass
+echo "bandit 4 password is: $(<./pass)"
 
 #level 4-5
 #password is stored in the only human-readable file
@@ -62,24 +60,24 @@ ban4(){
 	find . -type f -exec file {} + | grep ASCII
 }
 sshAccess 4 "$(typeset -f ban4); ban4"
-sshCopy 4 /home/bandit4/inhere/./-file07 ./otw/tmp
-mv ./otw/tmp/./-file07 ./otw/tmp/pass
-echo "bandit 5 password is: $(<./otw/tmp/pass)" 
+sshCopy 4 /home/bandit4/inhere/./-file07 ./
+mv ././-file07 ./pass
+echo "bandit 5 password is: $(<./pass)" 
 
 #level 5-6
 #password has properties: human-readable, 1033 bytes in size, not executable
 sshAccess 5 'find -type f -size 1033c ! -executable'
-sshCopy 5 /home/bandit5/inhere/maybehere07/.file2 ./otw/tmp
-mv ./otw/tmp/.file2 ./otw/tmp/pass
-echo "bandit 6 password is:  $(<./otw/tmp/pass)"
+sshCopy 5 /home/bandit5/inhere/maybehere07/.file2 ./
+mv ./.file2 ./pass
+echo "bandit 6 password is:  $(<./pass)"
 
 #level 6-7
 #password is somewhere in server has properties: owned by user bandit7, owned by group6, 33 bytes in size
 #forward slash used to search entire file system
 #2>dev/null redirects error messages ("Permission denied etc.") from terminal to /dev/null thats discarded by system
 sshAccess 6 'find / -type f -user bandit7 -group bandit6 -size 33c 2>/dev/null'
-sshCopy 6 /var/lib/dpkg/info/bandit7.password ./otw/tmp
-mv ./otw/tmp/bandit7.password ./otw/tmp/pass
-echo "bandit 7 password is $(<./otw/tmp/pass)"
+sshCopy 6 /var/lib/dpkg/info/bandit7.password ./
+mv ./bandit7.password ./pass
+echo "bandit 7 password is $(<./pass)"
 
-rm -r ./otw/tmp
+rm ./pass
