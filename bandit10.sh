3845e380d514f6ba152c36b7338a1e5187d13298
nikhil922 <nikhil922>
2020-07-26 19:20:09 +0000

    added bandit10.sh

---
+++ bandit10.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+USER=bandit10
+PASS=$(./bandit9.sh)
+PORT=2220
+HOST=bandit.labs.overthewire.org
+
+sshpass -p $PASS ssh -qp $PORT $USER@$HOST 'base64 -d data.txt | grep -Eo "\<.{32}\>"'
+ 
+
+(>&2 echo "Completed $USER")
