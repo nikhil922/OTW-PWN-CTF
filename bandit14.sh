f2923190711052c5f8e10f5d80c40fafb2d782c5
nikhil922 <nikhil922>
2020-08-02 21:15:51 +0000

    replaced sed with grep to make scripts more efficient

--- bandit14.sh
+++ bandit14.sh
@@ -5,6 +5,6 @@ PASS=$(./bandit13.sh)
 PORT=2220
 HOST=bandit.labs.overthewire.org
 
-sshpass -p $PASS ssh -qp $PORT $USER@$HOST "echo $PASS | nc localhost 30000" | sed -n '2p'
+sshpass -p $PASS ssh -qp $PORT $USER@$HOST "echo $PASS | nc localhost 30000" | grep -Eo "\<.{32}\>" 
  
 (>&2 echo "Completed $USER")
--- bandit15.sh
+++ bandit15.sh
@@ -2,9 +2,10 @@
 
 USER=bandit15
 PASS=$(./bandit14.sh)
+
 PORT=2220
 HOST=bandit.labs.overthewire.org
 
-sshpass -p $PASS ssh -qp $PORT $USER@$HOST "echo $PASS | openssl s_client -connect localhost:30001 -ign_eof" | sed -n '65p'
+sshpass -p $PASS ssh -qp $PORT $USER@$HOST "echo $PASS | openssl s_client -connect localhost:30001 -ign_eof" 2>/dev/null | grep -Eo "\<.{32}\>" | tail -1
 
 (>&2 echo "Completed $USER")
